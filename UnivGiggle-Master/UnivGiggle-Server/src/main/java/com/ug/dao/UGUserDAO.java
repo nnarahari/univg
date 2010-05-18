5a9642094c23c459ad71e4fa2f514200980215b2
seeni.vasan <seeni.vasan@e304af4f-777b-fba9-8207-9717958287e1>
2010-05-18 16:11:13 +0000

    Initial project files added.

---
+++ UnivGiggle-Master/UnivGiggle-Server/src/main/java/com/ug/dao/UGUserDAO.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.ug.dao;
+
+import com.ug.model.UG_User;
+
+/**
+ * @author srajen03
+ *
+ */
+public interface UGUserDAO {
+	public UG_User addUser(UG_User user) throws Exception;
+	public boolean validateUser(String userName, String password)throws Exception;
+	boolean updateUser(String emailId) throws Exception;
+}
