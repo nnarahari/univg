fef99372598871007dd2ab1d9592b381c0140fce
its.anil.joe@gmail.com <its.anil.joe@gmail.com@e304af4f-777b-fba9-8207-9717958287e1>
2010-07-25 09:14:52 +0000

    AddMentee without relation with Mentor.

--- trunk/UnivGiggle-Master/UnivGiggle-Server/src/test/java/com/ug/service/impl/MenteeManagerImplTest.java
+++ trunk/UnivGiggle-Master/UnivGiggle-Server/src/test/java/com/ug/service/impl/MenteeManagerImplTest.java
@@ -12,7 +12,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.ug.model.Mentee;
+import com.ug.model.Mentor;
 import com.ug.service.MenteeManager;
+import static org.junit.Assert.*;
 
 public class MenteeManagerImplTest {
 	private Logger logger = Logger.getLogger(MenteeManagerImplTest.class);
@@ -30,4 +32,41 @@ public class MenteeManagerImplTest {
 		List<Mentee> menteeList = menteeManager.getAvailableMentees();
 		assertTrue(menteeList.size()>0);
 	}
+	
+	@Test
+	public void testAddMenteeNoMentor(){
+		Mentee mentee = createMentee("Mentee1", "002","mentee002@ug.com");
+		mentee.setMentor(null);
+		try {
+			menteeManager.addMentee(mentee, null, null);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Exception occured "+e.getMessage());
+		}
+		Mentee returnedMentee = menteeManager.getMentee("mentee002@ug.com");
+		assertNotNull(returnedMentee);
+	}
+	
+	private Mentor createMentor(String fname,String lname, String email){
+		Mentor mentor = new Mentor(fname, lname, email);
+		mentor.setAge("22");
+		mentor.setCitizenship("India");
+		mentor.setDegree("MBA");
+		mentor.setGender("Male");
+		mentor.setLanguage("English");
+		mentor.setProfession("Software engr.");
+		return mentor;
+	}
+	
+	private Mentee createMentee(String fname,String lname, String email){
+		Mentee mentee = new Mentee(fname, lname, email);
+		mentee.setAge("22");
+		mentee.setCitizenship("India");
+		mentee.setDegree("MBA");
+		mentee.setGender("Male");
+		mentee.setLanguage("English");
+		mentee.setProfession("Student");
+		return mentee;
+	}
+
 }
