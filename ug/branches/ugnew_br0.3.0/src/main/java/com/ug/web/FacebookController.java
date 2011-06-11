202b76c294893dda730b283053870a10457802b7
seeni.vasan@gmail.com <seeni.vasan@gmail.com@e304af4f-777b-fba9-8207-9717958287e1>
2011-06-11 18:09:21 +0000

    Facebook login issue fixed.

--- ug/branches/ugnew_br0.3.0/src/main/java/com/ug/web/FacebookController.java
+++ ug/branches/ugnew_br0.3.0/src/main/java/com/ug/web/FacebookController.java
@@ -1,5 +1,12 @@
 package com.ug.web;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
 import org.springframework.social.facebook.api.FacebookApi;
 import org.springframework.social.facebook.api.impl.FacebookTemplate;
 import org.springframework.social.facebook.web.FacebookCookieValue;
@@ -12,22 +19,39 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class FacebookController {
-	/*@RequestMapping(value="/", method=RequestMethod.GET)
-	public ModelAndView home() {
-		return new ModelAndView("home");
-	}*/
+	private Logger logger = Logger.getLogger(FacebookController.class);
 	
 	@RequestMapping(value="auth.do")
-	public ModelAndView authenticate(@FacebookCookieValue(required=false, value="access_token") String cookie){
-		System.out.println("Auth Started...");
-		
+	public ModelAndView authenticate(@FacebookCookieValue(required=false, value="access_token") String cookie, HttpServletRequest req){
+		logger.debug("Auth Started... user" + req.getRemoteUser());
+		logger.debug("req ==>"+ req);
+		String value ="";
 		if(cookie == null){
-			return new ModelAndView("authfail");
+			logger.debug("cookie is null, getting accesstoken manually....");
+
+			for(Cookie cook :req.getCookies())
+			{
+				value = cook.getValue();
+				logger.debug(cook.getName()+ "==" + value);
+
+				if(cook.getValue().contains("access_token")){
+					cookie = value.substring(value.indexOf("=")+1, value.indexOf("&"));
+					try {
+						cookie = URLDecoder.decode(cookie, "UTF-8");
+					} catch (UnsupportedEncodingException e) {
+						e.printStackTrace();
+					}
+					break;	
+				}
+			}
+			logger.debug("decoded cookie ==>"+ cookie);
+
 		}
-		
+
+		logger.debug("cookie ==>"+ cookie);
 		FacebookApi facebook = new FacebookTemplate(cookie);
 		ModelAndView mav = new ModelAndView("authenticated");
-		System.out.println("FB Authentication success...");
+		System.out.println("FB Authentication success..." + facebook.userOperations().getUserProfile().getEmail());
 		mav.addObject("picture", facebook.userOperations().getUserProfile().getId());
 		mav.addObject("userProfile",facebook.userOperations().getUserProfile());
 		return mav;
