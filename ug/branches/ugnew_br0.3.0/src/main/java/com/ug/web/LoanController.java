2eab093a2062c6d82526b8ffdacbd2ea257428b3
seeni.vasan@gmail.com <seeni.vasan@gmail.com@e304af4f-777b-fba9-8207-9717958287e1>
2011-06-12 17:10:39 +0000

    download loan template file

--- ug/branches/ugnew_br0.3.0/src/main/java/com/ug/web/LoanController.java
+++ ug/branches/ugnew_br0.3.0/src/main/java/com/ug/web/LoanController.java
@@ -1,21 +1,53 @@
 package com.ug.web;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.ug.domain.Loan;
+
+import org.apache.log4j.Logger;
 import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.support.ByteArrayMultipartFileEditor;
 
 @RooWebScaffold(path = "loans", formBackingObject = Loan.class)
 @RequestMapping("/loans")
 @Controller
 public class LoanController {
+	
+	private  Logger logger = Logger.getLogger(LoanController.class);
+	
 	@InitBinder
     public void initBinder(WebDataBinder binder)
     {
         binder.registerCustomEditor(byte[].class, new ByteArrayMultipartFileEditor());
     }
+	
+	
+	@RequestMapping(value = "/getLoanTemplate",method=RequestMethod.GET)
+	public String getLoanTemplate(HttpServletRequest req, HttpServletResponse res){
+		logger.debug("getLoanTemplate() started...");
+		File file = new File("C:/ug/template/LoanTemplate.pdf"); 
+    	
+    	res.setContentType("pdf");
+    	res.setContentLength((int) file.length());
+    	res.setHeader("Content-Disposition","attachment; filename=\"" + file.getName() +"\"");
+    	try {
+			FileCopyUtils.copy(new FileInputStream(file), res.getOutputStream());
+		} catch (IOException e) {
+			logger.error("Error during file dowonload.....",e);
+		}
+		return null;
+		
+	}
 
 }
