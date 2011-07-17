0f0e26e1170ae968baa30c23e054dca62c5ba0e1
seeni.vasan@gmail.com <seeni.vasan@gmail.com@e304af4f-777b-fba9-8207-9717958287e1>
2011-07-17 07:41:40 +0000

    Mentor link added in student's page.

--- ug/branches/ugnew_br0.3.0/src/main/java/com/ug/web/MentorController.java
+++ ug/branches/ugnew_br0.3.0/src/main/java/com/ug/web/MentorController.java
@@ -1,12 +1,29 @@
 package com.ug.web;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.ug.domain.Mentor;
+
+import org.apache.log4j.Logger;
 import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RooWebScaffold(path = "mentors", formBackingObject = Mentor.class)
 @RequestMapping("/mentors")
 @Controller
 public class MentorController {
+	
+	private Logger logger = Logger.getLogger(MentorController.class);
+	
+	@RequestMapping(value="/mentorForStudent",method=RequestMethod.GET)
+	public String showMentros(HttpServletRequest req,HttpServletResponse res, Model uiModel){
+		logger.debug("showMentros() started...");
+		uiModel.addAttribute("mentors", null);
+		return "mentors/mentorForStudent";
+	}
 }
